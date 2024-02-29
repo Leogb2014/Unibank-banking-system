3c0114e46c8aa0729645a10a11cb0a8ef7611b29
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-02-29 13:59:26 -0300

    Client entity

---
+++ src/main/java/com/Leonardo/Unibank/unibank/entities/Client.java
@@ -0,0 +1,73 @@
+package com.Leonardo.Unibank.unibank.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name="clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String document;
+    private Instant birthDate;
+
+    public Client(){
+    }
+
+    public Client(Long id, String name, String document, Instant birthDate) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
